a8a8eae4c5a2577868b42578aa72db68a3a937f5
Rabchik2006 <rabcenco.valera@gmail.com>
2024-03-06 17:27:10 +0300

    fix

--- togif.sh
+++ togif.sh
@@ -7,6 +7,6 @@ filters="fps=$6,scale=$5:-1:flags=lanczos"
 ffmpeg -v warning -i "$1" -vf "$filters,palettegen" -y "$palette"
 ffmpeg -v warning -i "$1" -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y "$temp_gif"
 gifsicle -O3 --lossy=100 "$temp_gif" -o "$4"
-
+rm temp_gif palette
 
 
